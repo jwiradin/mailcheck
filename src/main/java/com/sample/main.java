ff67c844f2702b49f929dc98ad47c928c7f0a6e6
Lee <Julius.Lee@pega.com>
2022-05-03 16:42:45 +1000

    wip

--- src/main/java/com/sample/main.java
+++ src/main/java/com/sample/main.java
@@ -1,7 +1,7 @@
 package com.sample;
 
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
+import org.apache.commons.cli.*;
+import org.apache.commons.cli.ParseException;
 
 import javax.mail.*;
 import javax.mail.internet.*;
@@ -11,85 +11,138 @@ public class main {
 
     public static void main(String[] args) {
         Options options = new Options();
-        options.addOption(new Option("t", true, "mail to"));
-        options.addOption(new Option("f", true, "mail from"));
-        options.addOption(new Option("s", true, "smtp"));
-        options.addOption(new Option("p", true, "smtp port"));
-        options.addOption(new Option("u", true, "smtp user"));
-        options.addOption(new Option("p", true, "smtp password"));
-        options.addOption(new Option("l", true, "start tls"));
+        options.addOption(Option.builder("t")
+            .longOpt("mailto")
+            .hasArg(true)
+            .desc("mail to")
+            .required(true)
+            .build()
+        );
+        options.addOption(Option.builder("f")
+                .longOpt("mailfrom")
+                .hasArg(true)
+                .desc("mail from")
+                .required(true)
+                .build()
+        );
+        options.addOption(Option.builder("s")
+                .longOpt("smpthost")
+                .hasArg(true)
+                .desc("smtphost")
+                .required(true)
+                .build()
+        );
+        options.addOption(Option.builder("o")
+                .longOpt("smtpport")
+                .hasArg(true)
+                .desc("smtp port")
+                .required(true)
+                .build()
+        );
+        options.addOption(Option.builder("u")
+                .longOpt("user")
+                .hasArg(true)
+                .desc("smpt login id")
+                .build()
+        );
+        options.addOption(Option.builder("p")
+                .longOpt("password")
+                .hasArg(true)
+                .desc("smtp password")
+                .build()
+        );
+        options.addOption(Option.builder("l")
+                .longOpt("starttls")
+                .desc("starttls")
+                .build()
+        );
+        String to;
+        String from;
+        String host;
+        String username;
+        String password;
+        String starttls;
+        String port;
 
-
-        // change accordingly
-        String to = "got@gmail.com";
-
-        // change accordingly
-        String from = "akash@gmail.com";
-
-        // or IP address
-        String host = "localhost";
-
-        // mail id
-        final String username = "username@gmail.com"
-
-        // correct password for gmail id
-        final String password = "mypassword";
-
-        System.out.println("TLSEmail Start");
-        // Get the session object
-
+        CommandLineParser clp = new DefaultParser();
+        CommandLine cmd = null;
         // Get system properties
         Properties properties = System.getProperties();
 
-        // Setup mail server
-        properties.setProperty("mail.smtp.host", host);
-
-        // SSL Port
-        properties.put("mail.smtp.port", "465");
-
-        // enable authentication
-        properties.put("mail.smtp.auth", "true");
-
-        // SSL Factory
-        properties.put("mail.smtp.socketFactory.class",
-                "javax.net.ssl.SSLSocketFactory");
-
-        // creating Session instance referenced to
-        // Authenticator object to pass in
-        // Session.getInstance argument
-        Session session = Session.getDefaultInstance(properties,
-                new javax.mail.Authenticator() {
-
-                    // override the getPasswordAuthentication
-                    // method
-                    protected PasswordAuthentication
-                    getPasswordAuthentication() {
-                        return new PasswordAuthentication("username",
-                                "password");
-                    }
-                });
-
-
-//compose the message
         try {
-            // javax.mail.internet.MimeMessage class is mostly
-            // used for abstraction.
-            MimeMessage message = new MimeMessage(session);
+            cmd = clp.parse(options, args);
 
-            // header field of the header.
-            message.setFrom(new InternetAddress(from));
+            if (cmd.hasOption("t")) {
+                to = cmd.getOptionValue("t");
+            }
 
-            message.addRecipient(Message.RecipientType.TO,
-                    new InternetAddress(to));
-            message.setSubject("subject");
-            message.setText("Hello, aas is sending email ");
+            if (cmd.hasOption("f")) {
+                from = cmd.getOptionValue("f");
+            }
 
-            // Send message
-            Transport.send(message);
-            System.out.println("Yo it has been sent..");
-        } catch (
-                MessagingException mex) {
-            mex.printStackTrace();
+            if (cmd.hasOption("h")) {
+                properties.setProperty("mail.smtp.host", cmd.getOptionValue("l"));
+            }
+            if (cmd.hasOption("o")) {
+                properties.put("mail.smtp.port", cmd.getOptionValue("o"));
+            }
+            if (cmd.hasOption("u")) {
+                username = cmd.getOptionValue("u");
+            }
+
+            if (cmd.hasOption("p")) {
+                password = cmd.getOptionValue("p");
+            }
+
+            System.out.println("TLSEmail Start");
+            // enable authentication
+            properties.put("mail.smtp.auth", "true");
+
+            // SSL Factory
+            properties.put("mail.smtp.socketFactory.class",
+                    "javax.net.ssl.SSLSocketFactory");
+
+            // creating Session instance referenced to
+            // Authenticator object to pass in
+            // Session.getInstance argument
+            Session session = Session.getDefaultInstance(properties,
+                    new javax.mail.Authenticator() {
+
+                        // override the getPasswordAuthentication
+                        // method
+                        protected PasswordAuthentication
+                        getPasswordAuthentication() {
+                            return new PasswordAuthentication("username",
+                                    "password");
+                        }
+                    });
+
+
+            //compose the message
+            try {
+                // javax.mail.internet.MimeMessage class is mostly
+                // used for abstraction.
+                MimeMessage message = new MimeMessage(session);
+
+                // header field of the header.
+                message.setFrom(new InternetAddress(from));
+
+                message.addRecipient(Message.RecipientType.TO,
+                        new InternetAddress(to));
+                message.setSubject("subject");
+                message.setText("Hello, aas is sending email ");
+
+                // Send message
+                Transport.send(message);
+                System.out.println("Yo it has been sent..");
+            } catch (
+                    MessagingException mex) {
+                mex.printStackTrace();
+            }
         }
+        catch (ParseException ex) {
+            System.out.println(ex.getMessage());
+        }
+
     }
 }
